41f9302d7bba0115ffd06fe012cb26665d2aac0e
tmontanaro <teodoro.montanaro@unisalento.it>
2025-04-04 11:17:00 +0100

    working version that sends data to the server

--- firstApp.js
+++ firstApp.js
@@ -1,4 +1,5 @@
 const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
+const http = require('node:http');
 
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
@@ -13,8 +14,43 @@ process.exit(1);
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
 setInterval(function() {
-var readout = sensorLib.read();
+	var readout = sensorLib.read();
 
-console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+	console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
+	console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+	var temperature = readout.temperature.toFixed(1);
+
+	const postData = JSON.stringify({
+		'sensor': 'ID1',
+		'timestamp': 12345678,
+		'temperature': temperature
+	})
+	const options = {
+	hostname: '192.168.137.1',
+	port: 3000,
+	path: '/temperature',
+	method: 'POST',
+	headers: {
+	      'Content-Type': 'application/json',
+	      'Content-Length': Buffer.byteLength(postData),
+	},
+	};
+
+	const req = http.request(options, (res) => {
+		res.setEncoding('utf8');
+		res.on('data', (chunk) => {
+		  console.log(`BODY: ${chunk}`);
+		});
+		res.on('end', () => {
+		  console.log('No more data in response.');
+		});
+	});
+
+	req.on('error', (e) => {
+		console.error(`problem with request: ${e.message}`);
+	});
+
+	// Write data to request body
+	req.write(postData);
+	req.end();
 }, 2000);
